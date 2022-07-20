6d3a685b9457cbc708da2ede2fb48066ef32a57d
Darrell Rose <darrell.marjoram@sage.com>
2022-07-20 17:53:33 +0100

    Enable swagger for release (#3)

--- Program.cs
+++ Program.cs
@@ -23,11 +23,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseAuthorization();
 
